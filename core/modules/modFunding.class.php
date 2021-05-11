68ab731d8a49453a3f003e02f10f91e8e249196f
Anthony Berton <34568357+bb2a@users.noreply.github.com>
2021-05-11 17:20:40 +0200

    add picto in left menu

--- core/modules/modFunding.class.php
+++ core/modules/modFunding.class.php
@@ -74,7 +74,7 @@ class modFunding extends DolibarrModules
 		// Name of image file used for this module.
 		// If file is in theme/yourtheme/img directory under name object_pictovalue.png, use this->picto='pictovalue'
 		// If file is in module/img directory under name object_pictovalue.png, use this->picto='pictovalue@module'
-		$this->picto = 'fundingicon@funding';
+		$this->picto = 'funding@funding';
 		// Define some features supported by module (triggers, login, substitutions, menus, css, etc...)
 		$this->module_parts = array(
 			// Set this to 1 if module has its own trigger directory (core/triggers)
@@ -323,6 +323,7 @@ class modFunding extends DolibarrModules
 			'fk_menu'=>'fk_mainmenu=funding', // '' if this is a top menu. For left menu, use 'fk_mainmenu=xxx' or 'fk_mainmenu=xxx,fk_leftmenu=yyy' where xxx is mainmenucode and yyy is a leftmenucode
 			'type'=>'left', // This is a Top menu entry
 			'titre'=>'ModuleFundingName',
+			'prefix' => img_picto('', $this->picto, 'class="paddingright pictofixedwidth"'),
 			'mainmenu'=>'funding',
 			'leftmenu'=>'funding',
 			'url'=>'/funding/fundingindex.php',
