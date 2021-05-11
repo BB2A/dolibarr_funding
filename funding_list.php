41f63b252c20839e1dfcc9a872979d593716a402
Anthony Berton <34568357+bb2a@users.noreply.github.com>
2021-05-11 11:59:14 +0200

    order in list is desc default

--- funding_list.php
+++ funding_list.php
@@ -114,7 +114,7 @@ $search_array_options = $extrafields->getOptionalsFromPost($object->table_elemen
 
 // Default sort order (if not yet defined by previous GETPOST)
 if (!$sortfield) $sortfield = "t.".key($object->fields); // Set here default search field. By default 1st field in definition.
-if (!$sortorder) $sortorder = "ASC";
+if (!$sortorder) $sortorder = "DESC";
 
 // Initialize array of search criterias
 $search_all = GETPOST('search_all', 'alphanohtml') ? trim(GETPOST('search_all', 'alphanohtml')) : trim(GETPOST('sall', 'alphanohtml'));
