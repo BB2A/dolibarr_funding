aca97b1a8344b56b01e711d5db4966335a1968bd
BB2A Anthony Berton <bertonanthony@gmail.com>
2022-03-01 16:07:23 +0100

    FIX substitution type

--- core/substitutions/functions_funding.lib.php
+++ core/substitutions/functions_funding.lib.php
@@ -60,6 +60,7 @@ function funding_completesubstitutionarray(&$substitutionarray, $langs, $object)
 		$substitutionarray['__FUNDING_REDEMPTION_NUMBER__'] = isset($object->redemption_number) ? $object->redemption_number : '';
 		$substitutionarray['__FUNDING_RETENTION__'] = isset($object->retention) ? ($object->retention == 1 ? $langs->trans("Yes") : $langs->trans("No")) : '';
 		$substitutionarray['__FUNDING_RETENTION_RATE__'] = isset($object->retention_rate) ? $object->retention_rate : '';
+		$type = $object->fetchType($object->fk_funding_type);
 		$substitutionarray['__FUNDING_TYPE__'] = isset($type->label) ? $type->label : '';
 		$substitutionarray['__FUNDING_PRE_STUDY__'] = isset($object->pre_study) ? ($object->pre_study == 1 ? $langs->trans("Yes") : $langs->trans("No")) : '';
 		$substitutionarray['__FUNDING_USER_COMM_ID__'] = isset($object->fk_user_comm) ? $object->fk_user_comm : '';
