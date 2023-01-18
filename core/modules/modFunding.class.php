108c0d470d0ec94883d7aa9fd9f33ad8be9f6278
Anthony Berton <anthony.berton@bb2a.fr>
2023-01-18 15:36:52 +0100

    CheckVersion

--- core/modules/modFunding.class.php
+++ core/modules/modFunding.class.php
@@ -66,7 +66,7 @@ class modFunding extends DolibarrModules
 		// Possible values for version are: 'development', 'experimental', 'dolibarr', 'dolibarr_deprecated' or a version string like 'x.y.z'
 		$this->version = '1.0.5';
 		// Url to the file with your last numberversion of this module
-		//$this->url_last_version = 'http://127.0.0.1/versionmodule.txt';
+		$this->url_last_version = 'https://www.dolibarr.bb2a.fr/versionmodulefunding.txt';
 		//$this->needUpdate = '1.0.1';
 
 		// Key used in llx_const table to save module status enabled/disabled (where FUNDING is value of property name of module in uppercase)
