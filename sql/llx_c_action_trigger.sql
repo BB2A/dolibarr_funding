91309193a13d4e811e79889fbdc0a3570db87830
Anthony Berton <anthony.berton@bb2a.fr>
2023-05-24 12:21:51 +0200

    NEW - Add trigger for coef an retention

--- sql/llx_c_action_trigger.sql
+++ sql/llx_c_action_trigger.sql
@@ -1,6 +1,6 @@
 INSERT INTO `llx_c_action_trigger` (`rowid`, `elementtype`, `code`, `label`, `description`, `rang`) VALUES (NULL, 'funding', 'FUNDING_CREATE', 'Funding create', 'A funding is created', '1830040');
 INSERT INTO `llx_c_action_trigger` (`rowid`, `elementtype`, `code`, `label`, `description`, `rang`) VALUES (NULL, 'funding', 'FUNDING_VALIDATE', 'Funding validate', 'A funding is validate', '1830041');
-INSERT INTO `llx_c_action_trigger` (`rowid`, `elementtype`, `code`, `label`, `description`, `rang`) VALUES (NULL, 'funding', 'FUNDING_UPDATE', 'Funding update', 'A funding is updated', '1830042');
+INSERT INTO `llx_c_action_trigger` (`rowid`, `elementtype`, `code`, `label`, `description`, `rang`) VALUES (NULL, 'funding', 'FUNDING_MODIFY', 'Funding update', 'A funding is updated', '1830042');
 INSERT INTO `llx_c_action_trigger` (`rowid`, `elementtype`, `code`, `label`, `description`, `rang`) VALUES (NULL, 'funding', 'FUNDING_DRAFT', 'Funding DRAFT', 'A funding is DRAFT', '1830043');
 INSERT INTO `llx_c_action_trigger` (`rowid`, `elementtype`, `code`, `label`, `description`, `rang`) VALUES (NULL, 'funding', 'FUNDING_ACCEPT', 'Funding accept', 'A funding is accept by organisation', '1830044');
 INSERT INTO `llx_c_action_trigger` (`rowid`, `elementtype`, `code`, `label`, `description`, `rang`) VALUES (NULL, 'funding', 'FUNDING_DENIED', 'Funding denied', 'A funding is denied by organisation', '1830045');
@@ -14,5 +14,11 @@ INSERT INTO `llx_c_action_trigger` (`rowid`, `elementtype`, `code`, `label`, `de
 INSERT INTO `llx_c_action_trigger` (`rowid`, `elementtype`, `code`, `label`, `description`, `rang`) VALUES (NULL, 'funding', 'FUNDING_LACK', 'Funding lack', 'A funding is lack', '1830053');
 INSERT INTO `llx_c_action_trigger` (`rowid`, `elementtype`, `code`, `label`, `description`, `rang`) VALUES (NULL, 'funding', 'FUNDING_LACKOK', 'Funding lack ok', 'A funding is lack ok', '1830054');
 INSERT INTO `llx_c_action_trigger` (`rowid`, `elementtype`, `code`, `label`, `description`, `rang`) VALUES (NULL, 'funding', 'FUNDING_EXTENSION', 'Funding extension', 'A funding is extension', '1830055');
+INSERT INTO `llx_c_action_trigger` (`rowid`, `elementtype`, `code`, `label`, `description`, `rang`) VALUES (NULL, 'funding', 'COEFFICIENT_CREATE', 'Funding coefficient create', 'A funding coefficient is created', '1830071');
+INSERT INTO `llx_c_action_trigger` (`rowid`, `elementtype`, `code`, `label`, `description`, `rang`) VALUES (NULL, 'funding', 'COEFFICIENT_VALIDATE', 'Funding coefficient validate', 'A funding coefficient is validate', '1830071');
+INSERT INTO `llx_c_action_trigger` (`rowid`, `elementtype`, `code`, `label`, `description`, `rang`) VALUES (NULL, 'funding', 'COEFFICIENT_MODIFY', 'Funding coefficient update', 'A funding coefficient is updated', '1830072');
+INSERT INTO `llx_c_action_trigger` (`rowid`, `elementtype`, `code`, `label`, `description`, `rang`) VALUES (NULL, 'funding', 'RETENTION_CREATE', 'Funding retention create', 'A funding retention is created', '1830080');
+INSERT INTO `llx_c_action_trigger` (`rowid`, `elementtype`, `code`, `label`, `description`, `rang`) VALUES (NULL, 'funding', 'RETENTION_VALIDATE', 'Funding retention validate', 'A funding retention is validate', '1830081');
+INSERT INTO `llx_c_action_trigger` (`rowid`, `elementtype`, `code`, `label`, `description`, `rang`) VALUES (NULL, 'funding', 'RETENTION_MODIFY', 'Funding retention update', 'A funding retention is updated', '1830082');
 
 --ADD Call trigger IN TABLE
